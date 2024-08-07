796f8271e67e20f6f167927b99fdd7eadd687bbe
vaibhavvvishalg <vaibhavvvishalg@gmail.com>
2024-08-08 01:07:52 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Route from './Route.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
       }
     ]
   }
