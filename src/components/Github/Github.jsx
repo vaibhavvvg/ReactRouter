4925c5cf4fb0174820d64e78cf466bbaa5b9c63e
vaibhavvvishalg <vaibhavvvishalg@gmail.com>
2024-08-08 03:41:25 +0530

    commit

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom"
 
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/vaibhavvvg')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, [])
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/vaibhavvvg')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
     
   return (
     <div className="text-3xl text-center font-bold text-gray-700">Github followers: {data.followers} 
@@ -21,3 +23,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubLoaderInfo = async () => {
+    const response = await fetch('https://api.github.com/users/vaibhavvvg')
+    return response.json()
+}
