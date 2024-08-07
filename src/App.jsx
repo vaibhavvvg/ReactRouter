f794aad8c803d0fdbb04882efe928f6983808dfc
vaibhavvvishalg <vaibhavvvishalg@gmail.com>
2024-08-08 00:02:34 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-500 p-4'>React router</h1>
+      
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import {Link, Navlink} from 'react-router-dom'
+import { Link, Navlink } from 'react-router-dom'
 
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Headerexport default function Header() {
+export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
@@ -41,15 +35,15 @@ export default Headerexport default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b {isActive ? "text-orange-700" : "text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
                                 </NavLink>
                             </li>
-                            
-                            
+
+
                         </ul>
                     </div>
                 </div>
