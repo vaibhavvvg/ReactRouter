e6a50b82b3a2cbfee10623321bee81b5b4868999
vaibhavvvishalg <vaibhavvvishalg@gmail.com>
2024-08-09 03:41:58 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/vaibhavvvg"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -44,7 +44,7 @@ export default function Footer() {
                                     </a>
                                 </li>
                                 <li>
-                                    <Link to="/" className="hover:underline">
+                                    <Link to="https://discord.com/users/vaibhavvvg" className="hover:underline">
                                         Discord
                                     </Link>
                                 </li>
@@ -71,8 +71,8 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        <a href="https://github.com/vaibhavvvg" className="hover:underline">
+                            Vaibhav Vishal Gupta
                         </a>
                         . All Rights Reserved.
                     </span>
